02d7b84eb265e8c9aa038540abb7992041e34b3b
13391847922@163.com <guoxiangfang@oneapm.com>
2018-12-20 10:55:35 +0800

    页面加下拉弹框

--- js/public.js
+++ js/public.js
@@ -1,18 +1,3 @@
-//JS监听浏览器文字大小代码
-//var docEl = document.documentElement,
-//    resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-//    recalc = function () {
-//        var clientWidth = docEl.clientWidth;
-//        if (!clientWidth) return;
-//        clientWidth = clientWidth > 750 ? 750 : clientWidth;
-//        docEl.style.fontSize = 36 * (clientWidth / 320) + 'px';
-//      alert("aaan");
-//    };
-//
-//// if (!document.addEventListener) return;
-//window.addEventListener(resizeEvt, recalc, false);
-////document.addEventListener('onload', recalc, false);
-//recalc();
 $(document).ready(function(){
   var _rem;
   // 设置HTML跟节点字号
@@ -32,25 +17,45 @@ $(document).ready(function(){
     el.removeClass("overhide");
     $(".teles").hide();
   }
-});
-
-$(function(){
-  $('#model').load('../jieMeng12142/oneTypeSelect.html');
-  $('.J_selcate_btn').on("click",function(){
-      $('.mark').removeClass('hide');
-      var A=$('.oneTypeBox'),speed = 400;
-      A.stop().animate({height:'10rem'},speed).on('touchmove',function(e) {
-        e.preventDefault()
-      });
-  });
-
-  $('.mark').on("click",function(){
-      $('.mark').addClass('hide');
-      var A=$('.oneTypeBox'),speed = 400;
-      A.stop().animate({height:'0rem'},speed).on('touchmove',function(e) {
+  typeSlect();
+  function typeSlect() {
+    var markHtml ='\
+    <div class="oneTypeBox typeIcon">\
+      <h2>解梦分类</h2>\
+      <ul class="type clear">\
+        <li><a href="#"><em></em><span>人物</span></a></li>\
+        <li><a href="#"><em></em><span>动物</span></a></li>\
+        <li><a href="#"><em></em><span>植物</span></a></li>\
+        <li><a href="#"><em></em><span>物品</span></a></li>\
+        <li><a href="#"><em></em><span>身体</span></a></li>\
+        <li><a href="#"><em></em><span>生活</span></a></li>\
+        <li><a href="#"><em></em><span>活动</span></a></li>\
+        <li><a href="#"><em></em><span>自然</span></a></li>\
+        <li><a href="#"><em></em><span>情爱</span></a></li>\
+        <li><a href="#"><em></em><span>鬼神</span></a></li>\
+        <li><a href="#"><em></em><span>建筑</span></a></li>\
+        <li><a href="#"><em></em><span>其他</span></a></li>\
+      </ul>\
+    </div>\
+    <div class="mark hide"></div>';
+    if($("#model").length > 0){
+      $('#model').append(markHtml);
+      $('.J_selcate_btn').on("click",function(){
+        $('.mark').fadeIn(400);
+        var A = $('.oneTypeBox'),speed = 400;
+        A.stop().animate({height:'10rem'},speed).on('touchmove',function(e) {
           e.preventDefault()
+        });
       });
-      // event.stopPropagation();
-  });
+      $('.mark').on("click",function(){
+        $('.mark').fadeOut(400);
+        var A=$('.oneTypeBox'),speed = 400;
+        A.stop().animate({height:'0rem'},speed).on('touchmove',function(e) {
+          e.preventDefault()
+        });
+        event.stopPropagation();
+      });
+    }
 
-})
+  }
+});
